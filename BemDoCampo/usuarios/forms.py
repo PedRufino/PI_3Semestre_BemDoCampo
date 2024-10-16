f29e5ab33c78bb2edeb50da5ca049c9d9be7c4f5
Pedro Rufino <pedro.rmn19@gmail.com>
2024-10-16 00:02:48 -0300

    [Feat]: Ajustando funcionalidades do profile

--- BemDoCampo/usuarios/forms.py
+++ BemDoCampo/usuarios/forms.py
@@ -1,13 +1,128 @@
 from allauth.account.forms import SignupForm
+from django.forms import fields
+from .models import Usuarios
 from django import forms
 
+
 class CustomSignupForm(SignupForm):
-    first_name = forms.CharField(max_length=30, label='Nome')
-    last_name = forms.CharField(max_length=30, label='Sobrenome')
+    first_name = forms.CharField(max_length=30, label="Nome")
+    last_name = forms.CharField(max_length=30, label="Sobrenome")
 
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
         user.save()
         return user
+
+
+from django import forms
+from .models import Usuarios
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Usuarios
+        fields = (
+            "user_id",
+            "nome",
+            "sobrenome",
+            "email",
+            "documento",
+            "contato",
+            "data_nascimento",
+            "cep",
+            "endereco",
+            "numero",
+            "bairro",
+            "cidade",
+            "estado",
+        )
+        widgets = {
+            'user_id': forms.HiddenInput({
+                'value':'{{ user_id }}'
+            }),
+            'nome': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'John',
+                'id': 'name-input'
+            }),
+            'sobrenome': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Snow',
+                'id': 'lastname-input'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': '@example.com',
+                'id': 'email-input'
+            }),
+            'documento': forms.TextInput(attrs={
+                'class': 'form-control',
+                'maxlength': '14',
+                'placeholder': 'CPF / CNPJ',
+                'id': 'document-input'
+            }),
+            'contato': forms.TextInput(attrs={
+                'class': 'form-control',
+                'maxlength': '14',
+                'placeholder': '(00) 00000-0000',
+                'id': 'tel-input'
+            }),
+            'data_nascimento': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'dd/mm/aaaa',
+                'id': 'date-input'
+            }),
+            'cep': forms.TextInput(attrs={
+                'class': 'form-control',
+                'maxlength': '8',
+                'placeholder': '00000-000',
+                'id': 'cep-input'
+            }),
+            'endereco': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Rua Jacinto Pinto',
+                'id': 'logradouro-input'
+            }),
+            'numero': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '69',
+                'id': 'num-endereco-input'
+            }),
+            'bairro': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'São João da Gala',
+                'id': 'bairro-input'
+            }),
+            'cidade': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Cubatão',
+                'id': 'cidade-input'
+            }),
+            'estado': forms.TextInput(attrs={
+                'class': 'form-control',
+                'maxlength': '2',
+                'placeholder': 'SP',
+                'id': 'estado-input'
+            }),
+        }
+
+    def __init__(self, *args, user_data=None, **kwargs):
+        self.user_data = user_data
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:
+            if isinstance(self.user_data, dict) and field in self.user_data:
+                self.fields[field].initial = self.user_data[field]
+    
+    def save(self, commit=True, using=None):
+        # Cria a instância do modelo com os dados do formulário, mas sem salvar ainda
+        instance = super().save(commit=False)
+
+        # Salvar no banco de dados correto baseado no valor de 'using'
+        if using == 'mongo':  # Salva no MongoDB se 'using' for 'mongo'
+            instance.save(using='mongo')
+        elif commit:
+            instance.save(using='default')  # Salva no banco padrão (SQLite ou outro definido no settings)
+
+        return instance
